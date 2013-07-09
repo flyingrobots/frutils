d51293ae303e72cecf4b10efcdd85a36225dacc0
James Ross <jamesr@z2.com>
2013-07-09 11:47:24 -0700

    Bug fixes when parsing required arguments.

--- args.rb
+++ args.rb
@@ -1,48 +1,23 @@
 # args.rb
-# ------------------------------------------------------------------------------
-# The MIT License (MIT)
-# 
-# Copyright (c) 2013 James Ross
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-# ------------------------------------------------------------------------------
-file_dir = File.expand_path File.dirname(__FILE__)
-require File.join file_dir, 'arg_types.rb'
-require File.join file_dir, 'log.rb'
+# Created by James Ross 2013/6/25
+# Copyright (c) Z2 2013. All rights reserved.
 
-module FlyingRobots
+file_dir = File.dirname(__FILE__)
+require File.join file_dir, 'arg_types.rb'
+
+module Zen
 
 class Args
 public
   #----------------------------------------------------------------------------
   def initialize(options = {})
-    @log = Log.new({
-      :name => "Args",
-      # :volume => Log::VOLUME_DEBUG
-    })
     @flags = {}
     @options = {}
     @types = Types.new
     @help_title = options[:help_title]
     @help_desc = options[:help_desc]
     @copyright = options[:copyright]
-    _describe_flag "help", "Displays the help message."
+    _describe_flag "help", "Displays the help message.", {}
     options[:flags].each { |f|
       _describe_flag f[:name], f[:desc], f[:options]
     }
@@ -50,8 +25,7 @@ public
 
   #----------------------------------------------------------------------------
   def describe_flag(name, description, options = {})
-    raise "The option 'help' is reserved and cannot be overwritten." if name == "help"
-    @log.info "Adding flag '#{name}' with options: ", options
+    raise "The option 'help' is reserved" if name == "help"
     _describe_flag name, description, options
   end
 
@@ -84,41 +58,42 @@ public
     context = CONTEXT_FLAG
     flag = nil
     args.each_with_index { |arg, index|
-      @log.debug "Args: arg[#{index}] = #{arg}"
       next_arg = args.size > index ? args[index + 1] : nil
       next_arg_is_value = next_arg ? !next_arg.start_with?("-") : false
       case context
       when CONTEXT_FLAG
         flag = _parse_flag arg
-        raise "Unknown option '#{arg}'. See help for available options (-h or --help)." if flag == nil
+        raise "Unknown option '#{arg}'" if flag == nil
         name = flag[:name]
         type = flag[:type]
         if type == :boolean
           @options[name.to_sym] = true
-          @log.debug "Args: @options[#{name}] << 'true'"
         else
           context = CONTEXT_VALUE
           if next_arg == nil or not next_arg_is_value
-            raise "Option '#{name}' requires #{(flag[:multi] ? "at least one" : "a")} value of type '#{type}'."
+            num = flag[:multi] ? "at least one" : "a"
+            raise "Option '#{name}' requires #{num} value of type '#{type}'"
           end
         end
       when CONTEXT_VALUE
         type = flag[:type]
         value = _parse_value arg, type
         name = flag[:name]
-        @options[name.to_sym] << value
-        @log.debug "Args: @options[#{name}] << #{value}."
+        if flag[:required] == true or not flag[:multi]
+          @options[name.to_sym] = value
+        else
+          @options[name.to_sym] << value
+        end
         if next_arg_is_value
           if not flag[:multi]
-            raise "Option '#{name}' expects a single value of type '#{type}'."
+            raise "Option '#{name}' expects a single value of type '#{type}'"
           end
         else
           context = CONTEXT_FLAG
         end
       end
     }
-    @log.debug "@options:", @options
-    raise "No options were specified. See help for useage (-h or --help)." if @options.size == 0
+    raise "No options were specified" if @options.size == 0
     _check_for_required_options
     @options
   end
@@ -148,24 +123,27 @@ private
   #----------------------------------------------------------------------------
   def _check_for_required_options
     return if @options[:help]
-    reqs = @flags.values.select { |f| f[:required] == true }
-    reqs.each { |f| raise "Missing required option '#{f[:long]}'. See help for details (-h or --help)." if @options[f[:name]] == nil }
+    @flags.values.each { |f| 
+      if f[:required] == true and @options[f[:name]] == nil
+        raise "Missing required option '#{f[:long]}'"
+      end
+    }
   end
 
   #----------------------------------------------------------------------------
-  def _describe_flag(name, description, options = {})
+  def _describe_flag(name, description, options)
+    opts = options || {}
     name_as_sym = name.to_sym
     flag = {
       :name => name_as_sym,
-      :short => options.key?(:short) ? options[:short] : "-" + name.chars.first,
-      :long => options.key?(:long) ? options[:long] : "--" + name,
+      :short => opts.key?(:short) ? opts[:short] : "-" + name.chars.first,
+      :long => opts.key?(:long) ? opts[:long] : "--" + name,
       :description => description,
-      :default => options[:default],
-      :type => options.key?(:type) ? options[:type] : :boolean,
-      :multi => options[:multi] == true,
-      :required => options[:required] == true
+      :default => opts[:default],
+      :type => opts.key?(:type) ? opts[:type] : :boolean,
+      :multi => opts[:multi] == true,
+      :required => opts[:required] == true
     }
-    @log.debug "Described flag: ", flag
     _set_default_value flag unless flag[:required]
     _validate_flag flag
     @flags[name_as_sym] = flag
@@ -178,7 +156,7 @@ private
     if not flag.key? :default or flag[:default] == nil
       type = flag[:type]
       if @types.class_of(type) == nil
-        raise "Option #{name} has a type '#{type}', which is an unsupported type (must be ':boolean', ':int', ':float', or ':string')"
+        raise "Option #{name} has a type '#{type}', which is unsupported"
       end
       default_value = @types.default_value(type)
       flag[:default] = default_value
@@ -194,7 +172,7 @@ private
       default_value_type = @types.type_of flag[:default]
       # Note: type_of always returns a symbol, so convert "type" to
       # a symbol as well for a fair comparison
-      if default_value_type != type.to_sym
+      if not flag[:required] and default_value_type != type.to_sym
         raise "Option '#{flag[:name]}' has a default value of type '#{default_value_type}', which does not match the specified type '#{type}'"
       end
     end
