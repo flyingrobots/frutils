ca7609a8a74f0fac1a64d72059d4eb5e3c36d5b0
James Ross <jkross@gmail.com>
2013-07-10 20:07:38 -0700

    WIP JSON serialization methods, recursive hashing
    WIP methods added to Obj to serialize objects to and from JSON strings.
    Also WIP improvements to Obj's to/from_hash methods, which will now work
    recursively on "non-primitive" types (ie, non-built in classes).

--- obj.rb
+++ obj.rb
@@ -22,17 +22,83 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 # ------------------------------------------------------------------------------
+require 'rubygems'
+require 'json'
+
 module FlyingRobots
    
 class Obj
+  @@direct_types = [
+    Array,
+    Bignum,
+    FalseClass,
+    Fixnum,
+    Float,
+    Hash,
+    Integer,
+    String,
+    TrueClass
+  ]
+
+public 
   #--------------------------------------------------------------------------
   def self.to_hash(object, options = {})
     object.instance_variables.inject({}) { |h, ivar|
       key = (options[:symbolic_keys] == true) ? ivar[1..-1].to_sym : ivar[1..-1].to_s
-      h[key] = object.instance_variable_get(ivar)
+      value = object.instance_variable_get(ivar)
+      if @@direct_types.include? value.class
+        h[key] = value
+      else
+        h[key] = to_hash value
+      end
       h
     }
   end
+
+  #--------------------------------------------------------------------------
+  def self.from_hash(object, h)
+    h.each_pair { |key, value|
+      key_sym = key.to_sym
+      if object.instance_variable_defined? key_sym
+        if value.class == Hash
+          ivar = object.instance_variable_get key_sym
+          if @@direct_types.include? ivar.class
+            object.instance_variable_set key_sym, value
+          else
+            from_hash object.instance_variable_get(key_sym), value
+          end
+        end
+      else
+        type = object.class
+        raise "Unrecognized key '#{key}' for objectect of type '#{type}'"
+      end
+    }
+    object
+  end
+
+  #--------------------------------------------------------------------------
+  def self.to_json(object, options = {})
+    if object.respond_to? :to_json
+      object.to_json options
+    else
+      h = to_hash object
+      if options[:pretty] == true
+        JSON.pretty_generate h
+      else
+        JSON.generate h
+      end
+    end
+  end
+
+  #--------------------------------------------------------------------------
+  def self.from_json(object, json_str)
+    if object.respond_to? :from_json
+      object.from_json json_str
+    else
+      from_hash object, JSON.load(json_str)
+    end
+  end
+
 
 end
 
